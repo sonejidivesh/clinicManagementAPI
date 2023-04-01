62445957f00933ab55546f18c5c2d57b882fc6a5
sonejidivesh <sonejikaran@gmail.com>
2023-04-01 19:35:07 +0100

    Add modification

--- PrescriptionGeneration/Model/Prescription.cs
+++ PrescriptionGeneration/Model/Prescription.cs
@@ -3,7 +3,6 @@
     public class Prescription
     {
         public int Id { get; set; }
-
         public string PatientName { get; set; }
         public int DoctorId { get; set; }
         public int AppointmentId { get; set; }
